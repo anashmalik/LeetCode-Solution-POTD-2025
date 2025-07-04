8b51be909b482384c4782f10555dae57baed85be
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-07-04 11:46:27 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& op) { 
+        int c=op.size(),t=0;
+        while(c--){
+            long long a ;
+            if(k>pow(2,c)){
+                 t+=op[c];
+                 k-=pow(2,c);
+            }
+        }
+        cout<<t;
+        t=t%25;
+        return 'a'+t;
+
+
+    }
+};
