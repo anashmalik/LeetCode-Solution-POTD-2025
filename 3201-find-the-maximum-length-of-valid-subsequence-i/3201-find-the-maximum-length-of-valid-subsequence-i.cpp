1fd5853cd7c7f6d8c41baf28797082077b7f3ff2
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-07-16 22:29:00 -0700

    Time: 15 ms (24.22%), Space: 96.2 MB (39.32%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -8,15 +8,11 @@ public:
 
         for (int x : nums) {
             int new_dp[2][2] = {};
-
-            // Option 1: start a new sequence with one element (no q yet)
-            new_dp[x][0] = max(new_dp[x][0], 1);
+            new_dp[x][0] = 1;
             ans = max(ans, new_dp[x][0]);
-
-            // Option 2: extend existing sequences
             for (int p = 0; p < 2; ++p) {
                 for (int q = 0; q < 2; ++q) {
-                    if (dp[p][q] > 0 && ((p + q) % 2 == (p + x) % 2)) {
+                    if ((p + q) % 2 == (p + x) % 2) {
                         new_dp[x][p] = max(new_dp[x][p], dp[p][q] + 1);
                         ans = max(ans, new_dp[x][p]);
                     }
