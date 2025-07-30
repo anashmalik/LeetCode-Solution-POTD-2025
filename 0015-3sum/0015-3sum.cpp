5ec287173f64cb3ba13bac72390557b2aee90c74
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-07-31 03:17:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,24 @@
-//Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
-                    j++;
+        set<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-2;i++){
+            int j=i+1,k=nums.size()-1;
+            while(j<k){
+                if(0 == nums[i]+nums[j]+nums[k]){
+                    ans.insert({nums[i],nums[j],nums[k]});
+                    break;
+                }
+                else if(0<nums[i]+nums[j]+nums[k]){
                     k--;
-                } else if (sum < target) {
+                }
+                else{
                     j++;
-                } else {
-                    k--;
                 }
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
+        vector<vector<int>> res(ans.begin(),ans.end());
+        return res;
     }
 };
