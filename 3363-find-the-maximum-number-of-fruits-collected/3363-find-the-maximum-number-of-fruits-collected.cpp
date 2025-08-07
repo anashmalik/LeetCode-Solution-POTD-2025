45561ad8ae2241ca49e8e9efe9114b9d2854f2d7
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-08-07 22:02:48 +0530

    Time: 25 ms (92.86%), Space: 137.1 MB (94.16%) - LeetHub

--- 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.cpp
+++ 3363-find-the-maximum-number-of-fruits-collected/3363-find-the-maximum-number-of-fruits-collected.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    int maxCollectedFruits(vector<vector<int>>& f) {
-        int ans=0,n=f.size();
-        for(int i=0;i<n;i++){
-            ans+=f[i][i];
-            f[i][i]=0;
-        }
-        ans+=f[0][n-1]+f[n-1][0];
-        for(int i=1;i<n-1;i++){
-            if(i==n-2){
-                ans+=f[i][n-1];
-                ans+=f[n-1][i];
-            }
-            else{
-                ans+=max(f[i][n-2],f[i][n-1]);
-                ans+=max(f[n-2][i],f[n-1][i]);
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        int n = fruits.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i < j && j < n - 1 - i) fruits[i][j] = 0;
+                if (j < i && i < n - 1 - j) fruits[i][j] = 0;
             }
         }
-        return ans;
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            res += fruits[i][i];
+        }
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                fruits[i][j] += max(
+                    fruits[i - 1][j - 1] ,
+                    max(fruits[i - 1][j] , (j + 1 < n ? fruits[i - 1][j + 1] : 0))
+                );
+            }
+        }
+
+        // Third child (bottom-left to top-right)
+        for (int j = 1; j < n; ++j) {
+            for (int i = j + 1; i < n; ++i) {
+                fruits[i][j] += max(
+                    fruits[i - 1][j - 1] ,
+                    max(fruits[i][j - 1] , (i + 1 < n ? fruits[i + 1][j - 1] : 0))
+                );
+            }
+        }
+
+        return res + fruits[n - 1][n - 2] + fruits[n - 2][n - 1];
     }
 };
