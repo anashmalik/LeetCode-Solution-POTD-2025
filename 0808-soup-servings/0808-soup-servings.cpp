c101acebcb490172142c9e9706f1822442c18a8f
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-08-08 22:24:20 +0530

    Time: 189 ms (5.33%), Space: 640.2 MB (5.17%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-double operation(int a , int b ,  vector<vector<double>> &dp){
-    if(a <= 0 && b <= 0) return 0.5;
-    if(b <= 0) return 0;
-    if(a <= 0) return 1;
-    if(dp[a][b] != -1) return dp[a][b];
-
-    double ans =0;
-    ans += operation(a-100,b,dp); 
-    ans += operation(a-75,b-25,dp); 
-    ans += operation(a-50,b-50,dp); 
-    ans += operation(a-25,b-75,dp); 
-    return dp[a][b] = ans*0.25;
-}
     double soupServings(int n) {
         if(n>10000) return 1;
-        vector<vector<double>> dp(n+1,vector<double>(n+1,-1));
-        return operation(n,n,dp);
-
+        vector<vector<double>>dp(n+1,vector<double>(n+1,-1));
+        return help(n,n,dp);
+    }
+    double help(int a,int b,vector<vector<double>>&dp){
+        if(a<=0 && b<=0)return 0.5;
+        if(a<=0)return 1.0;
+        if(b<=0)return 0;
+        if(dp[a][b]!=-1)return dp[a][b];
+        return dp[a][b]= (help(a-100,b,dp)+help(a-75,b-25,dp)+help(a-50,b-50,dp)+help(a-25,b-75,dp))*0.25;
     }
 };
