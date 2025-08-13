c5e959f57356af96893589961e2efa7f50224c1e
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-08-13 21:53:16 +0530

    Time: 40 ms (6.17%), Space: 6.1 MB (100%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.c
@@ -0,0 +1,19 @@
+bool isPowerOfThree(int n){
+if(n==1){
+    return true;
+}
+   else if(n<3){
+       return false;
+   }
+    else{
+        while(n>1){
+            if(n%3==0){
+                n=n/3;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+}
