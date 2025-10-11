fa5bbf9fb3379b4f076dfe4941f186b1e59bd558
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-10-11 23:34:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    long long maximumTotalDamage(vector<int>& power) {
+        vector<int>s;
+        for(auto x:power){
+            mp[x]++;
+            if(mp[x]==1)
+                s.push_back(x);
+        }
+        sort(s.begin(),s.end());
+        vector<long long>dp(s.size(),-1);
+        return help(s,0,dp);        
+    }
+    long long help(vector<int>&s,int i,vector<long long>&dp){
+        if(i>=s.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        long long ans=0;
+        ans = help(s,i+1,dp);
+
+        long long t= (long long) mp[s[i]];
+        long long v= s[i],k=i;
+        while(i<s.size() && s[i]<v+2){
+            i++;
+        }
+        ans = max(ans,help(s,i,dp)+(t*v));
+        return dp[k]= ans;
+    }
+};
