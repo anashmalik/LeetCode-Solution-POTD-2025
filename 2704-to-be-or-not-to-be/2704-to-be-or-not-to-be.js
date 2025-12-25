0331cf7223be608cd6a16116523af195dadb0353
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-12-25 19:50:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    const v=val;
+    return {
+        toBe:function (x){
+            if(x===v) return {"value":true};
+            else throw new Error("Not Equal");
+        },
+        notToBe:function (x){
+            if(x!==v) return {"value":true};
+            else throw new Error("Equal");
+        }
+    }
+    
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
