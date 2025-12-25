0fb89d462e9415efc69c7dc7e18280ef163f107c
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-12-25 19:38:24 +0530

    Time: 46 ms (35.9%), Space: 55.5 MB (26.79%) - LeetHub

---
+++ 2620-counter/2620-counter.ts
@@ -0,0 +1,15 @@
+function createCounter(n: number): () => number {
+    var count = n;
+    return function() {
+        // count++;
+        return count ++;
+    }
+}
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
