525e6cd748e867f0849122724942efff0162862e
Anash Malik <117440315+anashmalik@users.noreply.github.com>
2025-12-26 15:53:14 +0530

    Time: 57 ms (12.63%), Space: 54.1 MB (92.69%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    nums.forEach((i)=>{
+        init=fn(init,i);
+    })
+    return init;
+    
+};
